8532ce011d7be6f369c176e1e41988c72245863b
Dorian Da Silva <80294826+DorianDaSilva@users.noreply.github.com>
2022-08-12 12:32:05 -0400

    Update ManualHeader.js

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -1,53 +1,53 @@
-//Manually setting wallet connect button & functionality
+// //Manually setting wallet connect button & functionality
 
-import {useMoralis} from "react-moralis"
-import {useEffect} from "react"
+// import {useMoralis} from "react-moralis"
+// import {useEffect} from "react"
 
 
-export default function ManualHeader(){
+// export default function ManualHeader(){
 
-const{ enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } = useMoralis()
+// const{ enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } = useMoralis()
 
-useEffect(() => {
-    if (isWeb3Enabled) return
-    if (typeof window !== "underfined"){
-        if (window.localStorage.getItem("connected")){
-            enableWeb3()
-        }
-    }
+// useEffect(() => {
+//     if (isWeb3Enabled) return
+//     if (typeof window !== "underfined"){
+//         if (window.localStorage.getItem("connected")){
+//             enableWeb3()
+//         }
+//     }
 
-}, [isWeb3Enabled])
+// }, [isWeb3Enabled])
 
-useEffect(() => {
-    Moralis.onAccountChanged((account) => {
-        console.log(`Account changed to ${account}`)
-        if (account == null) {
-            window.localStorage.removeItem("connected")
-            deactivateWeb3()
-            console.log("Null account found")
-        }
-    })
-}, [])
+// useEffect(() => {
+//     Moralis.onAccountChanged((account) => {
+//         console.log(`Account changed to ${account}`)
+//         if (account == null) {
+//             window.localStorage.removeItem("connected")
+//             deactivateWeb3()
+//             console.log("Null account found")
+//         }
+//     })
+// }, [])
 
-    return(<div><br></br>
-    Welcome to the decentralized Raffle!<br></br><br></br>
-        {account ? (
-            <div>
-                Connected to {account.slice(0,6)}...{account.slice(account.length - 4)}
-            </div>
-        ) : (
-            <button
-                onClick={async () => {
-                    await enableWeb3()
-                    if(typeof window !== "undefined") {
-                        window.localStorage.setItem("connected", "inject")
-                    }
-                }}
-                disabled={ isWeb3EnableLoading }
-                >
-                    Connect
-                </button>
-        )}
-    </div>)
-}
+//     return(<div><br></br>
+//     Welcome to the decentralized Raffle!<br></br><br></br>
+//         {account ? (
+//             <div>
+//                 Connected to {account.slice(0,6)}...{account.slice(account.length - 4)}
+//             </div>
+//         ) : (
+//             <button
+//                 onClick={async () => {
+//                     await enableWeb3()
+//                     if(typeof window !== "undefined") {
+//                         window.localStorage.setItem("connected", "inject")
+//                     }
+//                 }}
+//                 disabled={ isWeb3EnableLoading }
+//                 >
+//                     Connect
+//                 </button>
+//         )}
+//     </div>)
+// }
 
